50f81b57f8fb6f1159bf0dccae9cdcacd944326b
Artem Pugachev <artempugachev1@gmail.com>
2017-08-29 23:50:25 +0300

    blank emojifier class

---
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -0,0 +1,15 @@
+package com.example.android.emojify;
+
+
+import android.content.Context;
+import android.graphics.Bitmap;
+
+public class Emojifier {
+
+    /**
+     * Detect faces on the image and log its count
+     * */
+    static void detectFaces(Context context, Bitmap bitmap) {
+
+    }
+}
